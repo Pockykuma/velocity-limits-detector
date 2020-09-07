0eae7fac02cb32e11a9440e69e8c9f1f8c167e79
Taixiong Song <pockykuma@gmail.com>
2020-09-06 23:50:10 -0400

    add fileInput component

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import FileInput from './FileInput'
+
+render(<FileInput />, document.getElementById('main'))
---
+++ react-typescript/src/FileInput.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import axios from 'axios'
+import CSS from 'csstype'
+
+const textareaStyles: CSS.Properties = {
+  backgroundColor: 'lightgrey',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+  width: '600px',
+  height: '500px',
+}
+
+export default class FileInput extends React.Component<{}, { output: string }> {
+  constructor(props) {
+    super(props)
+    this.state = { output: '' }
+    this.uploadFile = this.uploadFile.bind(this)
+  }
+
+  uploadFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    let file: File = event.target.files[0]
+
+    if (file) {
+      let data: FormData = new FormData()
+      data.append('file', file)
+      try {
+        const response = await axios.post(
+          'http://localhost:8080/validateLoads',
+          data
+        )
+        if (response.status === 200) {
+          this.setState({
+            output: response.data.toString(),
+          })
+        }
+      } catch (error) {
+        this.setState({
+          output: 'Worng format file.',
+        })
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="file" name="myFile" onChange={this.uploadFile} />
+        <br></br>
+        <br></br>
+        <textarea value={this.state.output} style={textareaStyles} />
+      </div>
+    )
+  }
+}
